6136dee49e464fb2d1be671da574847014a5e49e
mohamed alaa <m-aa2001@hotmail.com>
2024-11-11 16:10:39 +0200

    adjusted the arc function

--- ITI_MathTasks/ITI_MathTasks_Day1/Source.cpp
+++ ITI_MathTasks/ITI_MathTasks_Day1/Source.cpp
@@ -14,13 +14,13 @@ enum MouthDir {
 // 90 deg is the +y axis (-y axis is sfml)
 void updateArc(sf::ConvexShape& shape, const sf::Vector2f& center, float radius, float startAngle, float endAngle)
 {
-    int pointCount = shape.getPointCount()-1;
-    float deltaAngle = (startAngle - endAngle) / pointCount;
+    int pointCount = shape.getPointCount() - 1;
+    float deltaAngle = (startAngle - endAngle) / (pointCount - 1);
     deltaAngle *= (PI / 180);
     endAngle *= (PI / 180);
 
     sf::Vector2f point = center;
-    for (;pointCount > -1; pointCount--)
+    for (;pointCount > 0; pointCount--)
     {
         point.x = center.x + radius * cos(endAngle);
         // inverted the y axis
@@ -30,7 +30,7 @@ void updateArc(sf::ConvexShape& shape, const sf::Vector2f& center, float radius,
     }
     shape.setPoint(0, center);
 }
-void PackMan(float Time, sf::ConvexShape& shape, const sf::Vector2f& center, float radius, float mouthAngle, MouthDir mouthDir, float frequency = 0.1)
+void PackMan(float Time, sf::ConvexShape& shape, const sf::Vector2f& center, float radius, float mouthAngle, MouthDir mouthDir, float frequency = 0.2)
 {
     float startAngle = mouthAngle* abs(sin(Time* frequency * (180/PI)));
 
---
+++ README.md
@@ -0,0 +1,5 @@
+### PacMan
+
+- use Arrow keys or WASD to change pacman direction
+
+![](https://github.com/MAthabet/ITI-math_Tasks/blob/Day1_PacMan/PACMAN.gif)
